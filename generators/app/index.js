d1c70e97b244158fe2d10271e57effbab6b9e3b7
sodogan <itelligence.i.dogan@daimler.com>
2021-12-14 15:05:30 +0300

    second

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,40 +1,35 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+"use strict";
+const Generator = require("yeoman-generator");
+const yosay = require("yosay");
+
+const msg = yosay(`Welcome to the first-class generator!`);
 
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
-    this.log(
-      yosay(
-        `Welcome to the first-class ${chalk.red('generator-sodogan-test')} generator!`
-      )
-    );
+    this.log(msg);
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
+        type: "input",
+        name: "GREET",
+        message: "Would you like to greet me?",
+        default: "sodogan",
+      },
     ];
 
-    return this.prompt(prompts).then(props => {
+    return this.prompt(prompts).then((props) => {
       // To access props later use this.props.someAnswer;
       this.props = props;
+      console.log(props);
     });
   }
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+    this.fs.copyTpl(
+      this.templatePath(__dirname + "/templates/template.txt"),
+      this.destinationPath(`output.txt`),
+      this.props
     );
-  }
-
-  install() {
-    this.installDependencies();
   }
 };
